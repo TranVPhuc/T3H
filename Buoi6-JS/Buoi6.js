1dd612df2ef578ea63d7b4aea55619fb6ca8f9d8
TranVPhuc <tranvanphuc.2712@gmail.com>
2023-06-28 08:37:07 +0700

    Update B6

---
+++ Buoi6-JS/Buoi6.js
@@ -0,0 +1,71 @@
+// alert("Day la vi du external")
+// function showAlert(){
+//             alert("Vi du internal")
+//         }
+
+//3 tu khoa khoi tao bien
+//var
+//let
+//const
+
+//1.var/let va const
+//Tu khoa var dung de khai bao 1 bien va bien do co the thay
+//doi gia tri
+    // var a = 10;
+    // console.log(a)
+    // var a =10;
+    // console.log(a)
+
+    //let
+    //Tu khoa var dung de khai bao 1 bien va bien do co the thay
+    //doi gia tri
+    // let b = 40;
+    // console.log(b)
+    // b = 'bon muoi';
+    // console.log(b)
+
+    //const
+    //Tu khoa var dung de khai bao 1 bien va bien do khong 
+    //the thay doi gia tri
+    // const c = 100;
+    // console.log(c)
+    // c = 200;
+    // console.log(c)
+
+//Khi khoi tao bien bang tu khoa let,var thi khong nhat thiet
+//phai gan luon gia tri
+
+//Khi khoi tao bien bang tu khoa const thi phai gan luon
+//gia tri khoi tao
+
+//2.var va let/const (Nhom theo phien ban)
+//var co co che hoisting
+
+// console.log(a);
+// const  a = 10;
+// console.log(a)
+
+//scope
+//var: global scope
+// {
+//     var couse1 = "HTML";
+//     var couse2 = "CSS";
+//     var couse3 = "JS";
+
+//     console.log(couse1)
+//     console.log(couse2)
+//     console.log(couse3)
+// }
+
+
+var a = 10
+var b = '10'
+var d = 10
+var f = true
+var c = b * d - a
+var e = a && f
+console.log(a + d + b)
+
+// var paragraph = "Day la doan van"
+// var paragraph = 'Day la doan van'
+// var paragraph = `Day la doan van`
